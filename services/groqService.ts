c0b4d974719a4d6ebbc61b31da07b3661b22dddf
Useradel07 <153228188+Useradel07@users.noreply.github.com>
2026-04-15 10:58:10 +0200

    Delete services/groqService.ts

--- services/groqService.ts
+++
@@ -1,4 +0,0 @@
-// This service is deprecated. The application now uses the secure backend API at /api/generate
-export const rewriteTextWithGroq = async () => {
-  throw new Error("This function is deprecated. Please use the secure /api/generate endpoint.");
-};
